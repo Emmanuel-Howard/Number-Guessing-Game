c0d3c677a66602c4d867f25346542c3030a91a0b
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-01-20 14:56:58 -0600

    Final Code + README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Number Guessing Game (Python)
+A game where the User must guess the correct number, using hints. 
+
+## Description
+This program prompts the User to guess a number (1-100). The program will then tell the User if his/her guess is too high or too low, counting how many guesses it takes. Once he/she guesses the correct number, they win!
+
+## Author
+Emmanuel Howard - https://github.com/Emmanuel-Howard
--- main.py
+++ main.py
@@ -3,29 +3,32 @@
 # Import random module
 import random
 
+# Run the program and set guesses to 0
 run = True
 guesses = 0
 
-answer = int(random.randint(1, 101))
+answer = int(random.randint(1, 101))   # Set random int as answer
 
+# Main game loop
 while run:
 
-    guess = input("Guess a number between 1 - 100: ")
+    guess = input("Guess a number between 1 - 100: ")   # Prompt user input
 
+# Use a try / except statement to catch Value Errors
     try:
-        guess = int(guess)
-        if guess == answer:
+        guess = int(guess)   # Turn User input into int
+        if guess == answer:   # Print Win message
             print(f"{answer} is correct! YOU WIN!")
             print(f"Number of guesses: {guesses}")
             break
-        elif guess > answer:
+        elif guess > answer:  # If guess is bigger than answer
             print(f"{guess} is too big!")
-            guesses += 1
-        elif guess < answer:
+            guesses += 1      # Add +1 to guesses
+        elif guess < answer:  # If guess is bigger than answer
             print(f"{guess} is too small")
-            guesses += 1
+            guesses += 1      # Add +1 to guesses
         else:
-            print("Please guess between 1 - 100")
+            print("Please guess between 1 - 100")  # Catches error if number isn't in range
 
     except ValueError:
         print("Invalid Input. Please enter a number 1 - 100.")
